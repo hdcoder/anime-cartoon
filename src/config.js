13d1007ce6ff7912fdf158d9b31ff19a5796390a
c0dezer0 <mail2manish15@gmail.com>
2017-02-17 12:35:04 +0530

    no fix

--- src/config.js
+++ src/config.js
@@ -52,8 +52,8 @@ const config = {
     }
   },
   production: {
-    ip: process.env.IP || '127.0.0.1',
-    port: process.env.PORT || 3000,
+    ip: 'localhost',
+    port:  3000,
     mongo: {
       uri:'mongodb://admin:admin@ds135029.mlab.com:35029/anime-cartoon'
     }
